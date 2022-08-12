fdc559131fc2a6e1f0b6e0e1d5a67ffb4a571246
LucasGobatto <lucas.bisaio@live.com>
2022-08-11 22:16:04 -0300

    rebase flux

---
+++ src/rebase/rebase-task.js
@@ -0,0 +1,19 @@
+import { addTask, exec } from "../task.js";
+import { log } from "../log/log.js";
+
+export async function runRebaseTask(branchs) {
+  const [target, origin] = branchs;
+
+  const goToTargetBranch = addTask("git", ["checkout", target]);
+  const gitPull = addTask("git", ["pull"]);
+  const goToCurrentBranch = addTask("git", ["checkout", origin]);
+  const makeRebase = addTask("git", ["rebase", target]);
+
+  await exec(goToTargetBranch);
+  await exec(gitPull);
+  await exec(goToCurrentBranch);
+  await exec(makeRebase);
+
+  log.success(`Git flow finished successfully!`);
+  process.exit(0);
+}
