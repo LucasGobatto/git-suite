a407cbfae0b2994fcf3421e604c349d9470d7d72
LucasGobatto <lucas.bisaio@live.com>
2022-12-29 00:16:00 -0300

    enhance: remove help and editor flags from valid commands array

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -18,8 +18,6 @@ export const validCommands = [
   "-checkout-branch", //15
   "-cd", //16
   "-pull", //17
-  "--help", // 18
-  "--editor-vsc", //19,
 ];
 export const commitType = ["--ft", "--fx", "--e", "--c", "--d"];
 export const rebaseFlags = ["--rc", "--continue", "--ra", "--abort", "--rs", "--skip"];
