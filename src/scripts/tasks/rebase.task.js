cd92612a6a9b59480311cce050397fcf66fe5044
LucasGobatto <lucas.bisaio@live.com>
2026-05-31 23:08:05 -0300

    fix: rebase script

--- src/scripts/tasks/rebase.task.js
+++ src/scripts/tasks/rebase.task.js
@@ -28,15 +28,21 @@ export async function gitRebaseTask(args, rebaseFlag) {
     return [goToHeadBranch, gitPull, goToCurrentBranch, makeRebase];
   }
 
-  if (args.length > 1)
+  const hasAddFlag = args.some((arg) => VALID_COMMANDS[VALID_COMMAND_TYPES.ADD].includes(arg));
+
+  if (args.length > 1 && !hasAddFlag)
     throw new Error(
       `Invalid param ${args.slice(1).join(', ')}. Choose one flag to continue rebase \`-ra\`, \`-rs\` or \`-rc\` `,
     );
 
-  const flag = VALID_REBASE_FLAGS[rebaseFlag][0];
-
-  const gitAdd = addTask('git', ['add', '.']);
+  const flag = VALID_REBASE_FLAGS[rebaseFlag][1];
+  
   const gitRebase = addTask('git', ['rebase', flag]);
 
-  return [gitAdd, gitRebase];
+  if (hasAddFlag) {
+    const gitAdd = addTask('git', ['add', '.']);
+    return [gitAdd, gitRebase];
+  }
+
+  return gitRebase;
 }
